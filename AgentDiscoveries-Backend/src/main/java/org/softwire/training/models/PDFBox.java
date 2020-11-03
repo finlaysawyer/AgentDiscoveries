f4774713ba76deadf847d43f7358752137c56ed4
William Rothery <will.rothery@live.co.uk>
2020-11-03 15:15:06 +0000

    nothing done yet really

---
+++ AgentDiscoveries-Backend/src/main/java/org/softwire/training/models/PDFBox.java
@@ -0,0 +1,40 @@
+package org.softwire.training.models;
+
+public class PDFBox {
+
+    public void regionReader() {
+        //bring in the information
+    }
+
+    public void locationReader() {
+        //bring in the information
+    }
+
+    public void parse() {
+        //parse information
+    }
+
+    public void display() {
+        //download the document
+    }
+
+
+    //3 method-constructor
+    //1 read
+    //2 parse
+    //3 print
+        //dan//finding the information that the report submits ---SQL
+        //find where the information is stored --reportbase
+        //extract the information (from SQL re:region/location-report)
+            //create SQL commands
+        //getters and setters (might already exist)
+        //passing them into pdfreader - (class)
+        //pdfreader needs to collect and parse the info
+        //seperate method to export
+            //decide whether to just download, or display/download
+        //export as .pdf document
+    //front end
+        //create a button on locations report
+        //create a button on region report
+        //button calls pdfreader (class)
+}

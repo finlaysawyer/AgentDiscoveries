d42e3750d073de0d516fcf83d297f37137b290da
Finlay <finjsawyer@gmail.com>
2020-11-03 09:15:50 +0000

    Make longitude and latitude optional

--- AgentDiscoveries-Backend/src/main/resources/db/migration/V7__add_longlatitude.sql
+++ AgentDiscoveries-Backend/src/main/resources/db/migration/V7__add_longlatitude.sql
@@ -1,2 +1,2 @@
-ALTER TABLE locations ADD longitude Decimal(9,6);
-ALTER TABLE locations ADD latitude Decimal(8,6);
+ALTER TABLE locations ADD longitude Decimal(9,6) NULL;
+ALTER TABLE locations ADD latitude Decimal(8,6) NULL;

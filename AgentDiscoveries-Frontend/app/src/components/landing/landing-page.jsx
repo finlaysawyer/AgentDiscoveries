5ca833fc0c6c9fa37214072cf3ed47bf50218f01
Harvey <hj_martin@sky.com>
2020-11-04 14:55:30 +0000

    Backend jar in file for will

--- AgentDiscoveries-Frontend/app/src/components/landing/landing-page.jsx
+++ AgentDiscoveries-Frontend/app/src/components/landing/landing-page.jsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
+import { Container } from 'react-bootstrap/lib/tab';
 import london from 'D:\\BP\\CodeBootCamp\\RepoStore\\AgentDiscoveries\\AgentDiscoveries-Frontend\\app\\images\\london.jpg'
 import paris from 'D:\\BP\\CodeBootCamp\\RepoStore\\AgentDiscoveries\\AgentDiscoveries-Frontend\\app\\images\\paris.jpg'
 import singapore from 'D:\\BP\\CodeBootCamp\\RepoStore\\AgentDiscoveries\\AgentDiscoveries-Frontend\\app\\images\\Singapore.jpg'
-import * as Bootstrap from 'react-bootstrap'
+
 export default class Landing extends React.Component {
 
     render() {
@@ -10,14 +11,12 @@ export default class Landing extends React.Component {
             <body>
                 <div>
                     <h1> Welcome to Agent Discoveries </h1>
+                    <Container> 
                     <img src={london} width= '320px' height= '200px' alt="London" />
                     <img src={paris} width= '320px' height= '200px' alt="Paris" />
                     <img src={singapore} width= '320px' height= '200px' alt="Paris" />
-                    <Bootstrap.Container>
-                    <Row>
-                     <Col>1 of 1</Col>
-                    </Row>
-                    </Bootstrap.Container>
+                    </Container>
+                   
                 </div>
            </body>
         );

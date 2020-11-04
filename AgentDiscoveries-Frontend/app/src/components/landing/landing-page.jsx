b3c19f2113ef374c061247bdae6d1edf334720f4
Harvey <hj_martin@sky.com>
2020-11-04 15:41:45 +0000

    Images working in grid position

--- AgentDiscoveries-Frontend/app/src/components/landing/landing-page.jsx
+++ AgentDiscoveries-Frontend/app/src/components/landing/landing-page.jsx
@@ -11,11 +11,17 @@ export default class Landing extends React.Component {
             <body>
                 <div>
                     <h1> Welcome to Agent Discoveries </h1>
-                    <Container> 
-                    <img src={london} width= '320px' height= '200px' alt="London" />
-                    <img src={paris} width= '320px' height= '200px' alt="Paris" />
-                    <img src={singapore} width= '320px' height= '200px' alt="Paris" />
-                    </Container>
+                    <container>
+                    <img id="London" src={london} width= '320px' height= '200px' alt="London" />
+                    <img id="Paris" src={paris} width= '320px' height= '200px' alt="Paris" />
+                    <img id="Singapore" src={singapore} width= '320px' height= '200px' alt="Singapore" />
+                    <img id="1" src={singapore} width= '320px' height= '200px' alt="Singapore" />
+                    <img id="2" src={singapore} width= '320px' height= '200px' alt="Singapore" />
+                    <img id="3" src={singapore} width= '320px' height= '200px' alt="Singapore" />
+                    <img id="4" src={singapore} width= '320px' height= '200px' alt="Singapore" />
+                    <img id="5" src={singapore} width= '320px' height= '200px' alt="Singapore" />
+                    <img id="6" src={singapore} width= '320px' height= '200px' alt="Singapore" />
+                    </container>
                    
                 </div>
            </body>

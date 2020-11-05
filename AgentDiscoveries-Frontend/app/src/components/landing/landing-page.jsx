48fd407c495c27cd515d8f92b9463ba7d446866e
Harvey <hj_martin@sky.com>
2020-11-05 10:14:28 +0000

    Landing with three hovers working

--- AgentDiscoveries-Frontend/app/src/components/landing/landing-page.jsx
+++ AgentDiscoveries-Frontend/app/src/components/landing/landing-page.jsx
@@ -2,6 +2,12 @@ import * as React from 'react';
 import london from 'D:\\BP\\CodeBootCamp\\RepoStore\\AgentDiscoveries\\AgentDiscoveries-Frontend\\app\\images\\london.jpg';
 import paris from 'D:\\BP\\CodeBootCamp\\RepoStore\\AgentDiscoveries\\AgentDiscoveries-Frontend\\app\\images\\paris.jpg';
 import singapore from 'D:\\BP\\CodeBootCamp\\RepoStore\\AgentDiscoveries\\AgentDiscoveries-Frontend\\app\\images\\Singapore.jpg';
+import singapore from 'D:\\BP\\CodeBootCamp\\RepoStore\\AgentDiscoveries\\AgentDiscoveries-Frontend\\app\\images\\Singapore.jpg';
+import singapore from 'D:\\BP\\CodeBootCamp\\RepoStore\\AgentDiscoveries\\AgentDiscoveries-Frontend\\app\\images\\Singapore.jpg';
+import singapore from 'D:\\BP\\CodeBootCamp\\RepoStore\\AgentDiscoveries\\AgentDiscoveries-Frontend\\app\\images\\Singapore.jpg';
+import singapore from 'D:\\BP\\CodeBootCamp\\RepoStore\\AgentDiscoveries\\AgentDiscoveries-Frontend\\app\\images\\Singapore.jpg';
+import singapore from 'D:\\BP\\CodeBootCamp\\RepoStore\\AgentDiscoveries\\AgentDiscoveries-Frontend\\app\\images\\Singapore.jpg';
+import singapore from 'D:\\BP\\CodeBootCamp\\RepoStore\\AgentDiscoveries\\AgentDiscoveries-Frontend\\app\\images\\Singapore.jpg';
 
 export default class Landing extends React.Component {
 
@@ -29,7 +35,6 @@ export default class Landing extends React.Component {
                             <div class="text">Singapore</div>
                                     </div>
                                     </div>
-                        <img id="Singapore" src={singapore} width= '320px' height= '200px' alt="Singapore" />
                         <img id="1" src={singapore} width= '320px' height= '200px' alt="Singapore" />
                         <img id="2" src={singapore} width= '320px' height= '200px' alt="Singapore" />
                         <img id="3" src={singapore} width= '320px' height= '200px' alt="Singapore" />

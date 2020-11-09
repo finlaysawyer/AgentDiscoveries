8e43c281706c31183febec06467a2d02f6a2ad96
Harvey <hj_martin@sky.com>
2020-11-09 16:53:28 +0000

    Redirect works and back button also works

--- AgentDiscoveries-Frontend/app/src/components/submit-forms/success-message.jsx
+++ AgentDiscoveries-Frontend/app/src/components/submit-forms/success-message.jsx
@@ -1,4 +1,6 @@
+import {Button} from 'react-bootstrap';
 import * as React from 'react';
+import {Link} from 'react-router-dom';
 
 
 export default class LocationReportSubmit extends React.Component {
@@ -6,7 +8,10 @@ export default class LocationReportSubmit extends React.Component {
     render() {
         return (
             <div>
-                <h1>Test</h1>
+                <h1>Submission Successful</h1>
+                <Link to='/'>
+                    <Button type='submit'> Go back</Button>
+                </Link>
             </div>
         );
     }

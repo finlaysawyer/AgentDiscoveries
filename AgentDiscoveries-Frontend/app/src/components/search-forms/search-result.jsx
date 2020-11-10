eefe366dc402780aa80a64c5452d3c0aee2cccbb
William Rothery <will.rothery@live.co.uk>
2020-11-10 16:15:57 +0000

    proper fucked it.

--- AgentDiscoveries-Frontend/app/src/components/search-forms/search-result.jsx
+++ AgentDiscoveries-Frontend/app/src/components/search-forms/search-result.jsx
@@ -8,24 +8,79 @@ export default class SearchResult extends React.Component {
     render() {
         return (
             <div className='results'>
+                {this.renderView(this.props.results)}
                 {this.getResultsHeader(this.props.results)}
                 {this.renderResults(this.props.results)}
             </div>
         );
     }
 
+    onPageChange(event) {
+        this.setState({ page: parseInt(event.target.value) });
+    }
+
+    onViewChange(value) {
+        this.setState({ view: value });
+    }
+
     renderResults(results) {
         return results.map((result, index) => {
-            return (
-                <Panel id="resultsBox" key={index}>
-                    <Panel.Heading>Result</Panel.Heading>
-                    <Panel.Body>
-                        {this.renderResultBody(result)}
-                        <Button bsStyle="success" type="button" onClick={() => this.generatePdf(result[Object.keys(result)[0]])}>Export to PDF</Button>
-                    </Panel.Body>
-                </Panel>
-            );
+            if (index >= (this.state.page - 1) * 10 && index < this.state.page * 10) {
+                if (result.reportTitle == undefined) {      // REGION REPORT
+                    return (
+                        <Panel key={index}>
+                        <Panel.Heading>Result</Panel.Heading>
+                            <Panel.Body>
+                                {this.renderResultBody(result)}
+                                <Button bsStyle="success" type="button" onClick={() => this.generatePdf(result[Object.keys(result)[0]])}>Export to PDF</Button>
+                                <Button bsStyle="success" className="buttonSubmit" id='submit-view' onClick={() => this.onViewChange(result)} type='submit'>View</Button>
+                            </Panel.Body>
+                        </Panel>
+                    );
+                } else {                                    // LOCATION REPORT
+                    return (
+                        <Panel key={index}>
+                            <Panel.Heading id="reportId">{`ID: ${result.reportId}`}</Panel.Heading>
+                            <Panel.Body>
+                            {this.renderResultBody(result)}
+                                <Button bsStyle="success" type="button" onClick={() => this.generatePdf(result[Object.keys(result)[0]])}>Export to PDF</Button>
+                                <Button bsStyle="success" className="buttonSubmit" id='submit-view' onClick={() => this.onViewChange(result)} type='submit'>View</Button>
+                            </Panel.Body>
+                        </Panel>
+                    );
+                }
+            }
         });
+    }
+
+    renderView(result) {
+        if (result.reportTitle == undefined) {      // REGION REPORT VIEW
+            return (
+                <div id='resultView' ref={(ref) => this.viewElement = ref}>
+                    <Panel>
+                        <Panel.Heading>View Report</Panel.Heading>
+                        <Panel.Body>
+                        {this.renderResultBody(result)}
+                            <Button bsStyle="success" type="button" onClick={() => this.generatePdf(result[Object.keys(result)[0]])}>Export to PDF</Button>
+                            <Button bsStyle="success" className="buttonSubmit" id='submit-view' onClick={() => this.onViewChange(result)} type='submit'>View</Button>
+                        </Panel.Body>
+                    </Panel>
+                </div>
+            );
+        } else {                                    // LOCATION REPORT VIEW
+            return (
+                <div id='resultView' ref={(ref) => this.viewElement = ref}>
+                    <Panel>
+                        <Panel.Heading>View Report</Panel.Heading>
+                        <Panel.Body>
+                        {this.renderResultBody(result)}
+                            <Button bsStyle="success" type="button" onClick={() => this.generatePdf(result[Object.keys(result)[0]])}>Export to PDF</Button>
+                            <Button bsStyle="success" className="buttonSubmit" id='submit-view' onClick={() => this.onViewChange(result)} type='submit'>View</Button>
+                        </Panel.Body>
+                    </Panel>
+                </div>
+            );
+        }
     }
 
     generatePdf(id) {

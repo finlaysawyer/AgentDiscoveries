ccde3ea912477f3725328094f392d7c24ed706bd
Harvey <hj_martin@sky.com>
2020-11-11 10:33:18 +0000

    The Modal box is working and displaying the final result in the results

--- AgentDiscoveries-Frontend/app/src/components/search-forms/search-result.jsx
+++ AgentDiscoveries-Frontend/app/src/components/search-forms/search-result.jsx
@@ -34,10 +34,10 @@ export default class SearchResult extends React.Component {
                     <Panel.Body>
                         {this.renderResultBody(result)}
                         <Button bsStyle="success" type="button" onClick={() => this.generatePdf(result[Object.keys(result)[0]])}>Export to PDF</Button>
-                        <Button bsStyle="success" type="button" onClick={() =>this.handleModal()}>Open Modal</Button>
+                        <Button bsStyle="success" type="button" onClick={() =>this.handleModal(result[Object.keys(result)[0]])}>Open Modal</Button>
                         <Modal show={this.state.show} >
                             <Modal.Header>Modal Head Part</Modal.Header>
-                            <Modal.Body>This is the modal body</Modal.Body>
+                            <Modal.Body>{this.renderResultBody()}</Modal.Body>
                             <Modal.Footer>
                                 <Button bsStyle="success" type="button" onClick={() =>this.handleModal()}>
                             Close modal
